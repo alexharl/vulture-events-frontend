12832a898acf3eebbc3bac06b1237790087cf683
Alexander Harl <alexander.harl@curry-group.com>
2023-09-20 15:57:15 +0200

    pwa

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { PrimeReactProvider } from 'primereact/api';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 //theme
 import './index.css';
 import 'primereact/resources/themes/vela-orange/theme.css';
@@ -28,6 +30,11 @@ root.render(
     </PrimeReactProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
